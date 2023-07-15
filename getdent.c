0cf7f426b0e49a2114f5c07aa0b7a5cd3a6c4352
rnr <zacharytanhx@yahoo.com>
2023-07-15 23:54:10 +0800

    Moved relevant code to getdents.c, playing with makefile

---
+++ getdent.c
@@ -0,0 +1,74 @@
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+
+#define BUF_SIZE 128
+#include "datatypes.h"
+#include "dir.h"
+#include "paths.h"
+
+dir_t *pass, *fail, *extras;
+char *dir, *src, *dst;
+unsigned int *count;
+
+int getdent(char *name) {
+    int fd, nread;
+    char buf[BUF_SIZE];
+    struct linux_dirent *d;
+    int bpos;
+    char d_type;
+
+    fd = open(name, O_RDONLY | O_DIRECTORY);
+    if (fd == -1) goto error;
+
+    nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);
+    while (nread != 0) {
+        if (nread == -1) goto error;
+
+        for (bpos = 0; bpos < nread; bpos += d->d_reclen) {
+            d = (struct linux_dirent *) (buf + bpos);
+            if (!strcmp(d->d_name, ".") || !strcmp(d->d_name, "..")) continue;
+
+            d_type = *(buf + bpos + d->d_reclen - 1);
+            char path[PATH_MAX];
+            snprintf(path, sizeof(path), "%s/%s", name, (char *)d->d_name);
+
+            if(d_type == DT_DIR) {
+                getdent(path);
+            } else {
+                if (!strcmp(path, src) || !strcmp(path, dst)) continue;
+
+                add_path_to_dir(get_relative_path(dir, path) , extras);
+                (*count)++;
+            }
+        }
+
+        nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);
+    }
+
+    return 0;
+
+error:
+    printf("Something went wrong while reading the directory\n");
+    exit(-4);
+}
+
+
+// iterate through directories, path is absolute path
+void file_iterator(char *dirl, char *srcl, char *dstl,
+        dir_t *passl, dir_t *faill, dir_t *extral, unsigned int *countl) {
+    pass = passl;
+    fail = faill;
+    extras = extral;
+    dir = dirl;
+    src = srcl;
+    dst = dstl;
+    count = countl;
+
+    getdent(dir);
+}
--- makefile
+++ makefile
@@ -1,12 +1,21 @@
 CC=clang
-CF=$(CFLAGSD)
-CFLAGS=-c -Wall --optimize -I headers
-CFLAGSD=-c -Wall -g -fsanitize=address -I headers
-OBJECTS=args.o compare.o dir.o files.o hashing.o paths.o main.o
+CF=-c -Wall --optimize -I headers
+CFD=-c -Wall -g -fsanitize=address -I headers
+OBJECTS=args.o compare.o dir.o files.o getdent.o hashing.o paths.o main.o
+DBGOBJ=args-debug.o compare-debug.o dir-debug.o files-debug.o getdent-debug.o hashing-debug.o paths-debug.o main-debug.o
+SOURCE=args.c compare.c dir.c files.c getdent.c hashing.c paths.c main.c
+
 all: prog clean
 
 prog: $(OBJECTS)
-	$(CC) $(OBJECTS) -g -fsanitize=address -lcrypto -o main
+	$(CC) $(OBJECTS) -lcrypto -o main
+
+debug: prog-debug clean
+
+prog-debug: $(SOURCE)
+	foreach src, $(SOURCE), $(echo $(src))
+	#$(CC) $(SOURCE) -g -fsanitize=address -o $(SOURCE)-debug.o))
+	$(CC) $(DBGOBJ) -g -fsanitize=address -lcrypto -o main
 
 args.o:args.c
 	$(CC) $(CF) args.c
@@ -20,6 +29,9 @@ dir.o:dir.c
 files.o:files.c
 	$(CC) $(CF) files.c
 
+getdent.o:getdent.c
+	$(CC) $(CF) getdent.c
+
 hashing.o:hashing.c
 	$(CC) $(CF) hashing.c
 
@@ -31,6 +43,3 @@ main.o:main.c
 
 clean:
 	rm -rf *.o
-
-run:
-	 ./main .
