e2d2095cb6f8c160a2add7a5cd8ec83aa234ef94
Zachareee <zacharytanhx@yahoo.com>
2023-07-19 10:34:30 +0800

    Working on OS agnostic code

--- getdent.c
+++ getdent.c
@@ -1,13 +1,70 @@
 #include <dirent.h>
 #include <fcntl.h>
+#include <ftw.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
+#include <unistd.h>
 
+#ifdef __unix__
 #define BUF_SIZE 1024
+#define getdent __getdent_linux(name) {                                         \
+    int fd, nread;                                                              \
+    char buf[BUF_SIZE];                                                         \
+    struct linux_dirent *d;                                                     \
+    int bpos;                                                                   \
+    char d_type;                                                                \
+                                                                                \
+    fd = open(name, O_RDONLY | O_DIRECTORY);                                    \
+    if (fd == -1) goto error;                                                   \
+                                                                                \
+    nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);                           \
+    while (nread != 0) {                                                        \
+        if (nread == -1) goto error;                                            \
+                                                                                \
+        for (bpos = 0; bpos < nread; bpos += d->d_reclen) {                     \
+            d = (struct linux_dirent *) (buf + bpos);                           \
+            if (!strcmp(d->d_name, ".") || !strcmp(d->d_name, "..")) continue;  \
+                                                                                \
+            d_type = *(buf + bpos + d->d_reclen - 1);                           \
+            char path[PATH_MAX];                                                \
+            snprintf(path, sizeof(path), "%s/%s", name, (char *)d->d_name);     \
+                                                                                \
+            if(d_type == DT_DIR) {                                              \
+                getdent(path);                                                  \
+            } else {                                                            \
+                if (!strcmp(path, src) || !strcmp(path, dst)) continue;         \
+                if (path_exists(pass, get_relative_path(dir, path))             \
+                        || path_exists(fail, get_relative_path(dir, path)))     \
+                    continue;                                                   \
+                                                                                \
+                add_path_to_dir(get_relative_path(dir, path) , extras);         \
+                (*count)++;                                                     \
+            }                                                                   \
+        }                                                                       \
+                                                                                \
+        nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);                       \
+    }                                                                           \
+                                                                                \
+    return 0;                                                                   \
+                                                                                \
+error:                                                                          \
+    printf("Something went wrong while reading the directory\n");               \
+    exit(-4);                                                                   \
+}                                                                               \
+
+#endif
+
+#ifdef __APPLE__
+
+#define getdent (path) { \
+    nftw(path, &loop_files, 1, FTW_PHYS);\
+}
+
+#endif
+
 #include "datatypes.h"
 #include "dir.h"
 #include "paths.h"
@@ -17,50 +74,6 @@ char *dir, *src, *dst;
 unsigned int *count;
 
 // recursive function to get directories
-int getdent(char *name) {
-    int fd, nread;
-    char buf[BUF_SIZE];
-    struct linux_dirent *d;
-    int bpos;
-    char d_type;
-
-    fd = open(name, O_RDONLY | O_DIRECTORY);
-    if (fd == -1) goto error;
-
-    nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);
-    while (nread != 0) {
-        if (nread == -1) goto error;
-
-        for (bpos = 0; bpos < nread; bpos += d->d_reclen) {
-            d = (struct linux_dirent *) (buf + bpos);
-            if (!strcmp(d->d_name, ".") || !strcmp(d->d_name, "..")) continue;
-
-            d_type = *(buf + bpos + d->d_reclen - 1);
-            char path[PATH_MAX];
-            snprintf(path, sizeof(path), "%s/%s", name, (char *)d->d_name);
-
-            if(d_type == DT_DIR) {
-                getdent(path);
-            } else {
-                if (!strcmp(path, src) || !strcmp(path, dst)) continue;
-                if (path_exists(pass, get_relative_path(dir, path))
-                        || path_exists(fail, get_relative_path(dir, path))) continue;
-
-                add_path_to_dir(get_relative_path(dir, path) , extras);
-                (*count)++;
-            }
-        }
-
-        nread = syscall(SYS_getdents, fd, buf, BUF_SIZE);
-    }
-
-    return 0;
-
-error:
-    printf("Something went wrong while reading the directory\n");
-    exit(-4);
-}
-
 
 // iterate through directories, path is absolute path
 void file_iterator(char *dirl, char *srcl, char *dstl,
@@ -74,4 +87,17 @@ void file_iterator(char *dirl, char *srcl, char *dstl,
     count = countl;
 
     getdent(dir);
+}
+
+int loop_files(const char *path, const struct stat *sb, int type) {
+    // return if encounter directory
+    if (type != FTW_F) return 0;
+
+    // check if path exists, if not add to dir
+    if (path_exists(pass, get_relative_path(dir, path))
+            || path_exists(fail, get_relative_path(dir, path)))
+        continue;
+    add_path_to_dir(get_relative_path(dir, path) , extras);
+    (*count)++;
+    return 0;
 }
