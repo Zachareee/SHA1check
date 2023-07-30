77e70aee7ba754a5b2bbe41bb9180b125655d34b
rnr <zacharytanhx@yahoo.com>
2023-07-31 01:31:35 +0800

    Added obfuscated hash and size comparison

--- compare.c
+++ compare.c
@@ -39,10 +39,19 @@ void trim(char *line, int offset) {
     }
 }
 
+// obfuscates hash
+void obf_hash(char *line, char *result) {
+    strncpy(result, line, 10);
+    strcat(result, "***");
+    strncpy(result + 13, line + 30, 10);
+}
+
 // compare hash of file against recorded hash
 // returns -1 if no hash found, -2 if file not found
 // 0 if hashsum equal and positive if strcmp fails
 int compare(char *dir, char *line, int ver) {
+    int result;
+    char *hash_value;
     regmatch_t pmatch[1];
 
     if (ver == 1) {
@@ -68,6 +77,9 @@ int compare(char *dir, char *line, int ver) {
         // construct file_struct to give to hash function
         struct stat s;
         stat(path, &s);
+
+        hash_value = hash(path, s.st_size);
+        result = strcmp(hash_value, hex);
     } else {
         char hex[24];
         hex[23] = 0;
@@ -79,6 +91,8 @@ int compare(char *dir, char *line, int ver) {
 
         // trim line to find file
         trim(line, pmatch[0].rm_so);
+        long size;
+        sscanf(line + pmatch[0].rm_eo, "%ld", &size);
 
         // copy to path and concat
         char path[PATH_MAX];
@@ -91,10 +105,15 @@ int compare(char *dir, char *line, int ver) {
         // construct file_struct to give to hash function
         struct stat s;
         stat(path, &s);
+        hash_value = hash(path, s.st_size);
+
+        char temp[24];
+        obf_hash(hash_value, temp);
+        printf("Obfuscated hash: %s, expected size: %ld, real size: %ld\n", temp, size, s.st_size);
+        result = strcmp(temp, hex);
+        result |= s.st_size - size;
     }
 
-    char *hash_value = hash(path, s.st_size);
-    int result = strcmp(hash_value, hex);
     fprintf(stderr, "OK\n");
 
     // free malloc-ed variables
