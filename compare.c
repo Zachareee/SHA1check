1233aeb9d823165a93ea7c841efb4b2d661ddeee
rnr <zacharytanhx@yahoo.com>
2023-07-31 11:59:33 +0800

    zz

--- compare.c
+++ compare.c
@@ -42,7 +42,7 @@ void trim(char *line, int offset) {
 // obfuscates hash
 void obf_hash(char *line, char *result) {
     strncpy(result, line, 10);
-    strcat(result, "***");
+    strncpy(result + 10, "***", 3);
     strncpy(result + 13, line + 30, 10);
 }
 
@@ -50,67 +50,54 @@ void obf_hash(char *line, char *result) {
 // returns -1 if no hash found, -2 if file not found
 // 0 if hashsum equal and positive if strcmp fails
 int compare(char *dir, char *line, int ver) {
-    int result;
+    long result;
     char *hash_value;
+    char hex[41];
     regmatch_t pmatch[1];
+    int limit;
+    regex_t *reg;
 
     if (ver == 1) {
-        char hex[41];
-        hex[40] = 0;
-
-        // find hexstring and copy to hex, return -1 if no matches
-        int match = regexec(&reghex1, line, 1, pmatch, 0);
-        if (match) return -1;
-        strncpy(hex, line + pmatch[0].rm_so, 40);
-
-        // trim line to find file
-        trim(line, pmatch[0].rm_so);
-
-        // copy to path and concat
-        char path[PATH_MAX];
-        strcpy(path, line);
-        concat_path(dir, path);
-        if (!check_exists(path, 1)) return -2;
-
-        fprintf(stderr, "Checking %s...", get_relative_path(dir, path));
-
-        // construct file_struct to give to hash function
-        struct stat s;
-        stat(path, &s);
-
-        hash_value = hash(path, s.st_size);
-        result = strcmp(hash_value, hex);
+        limit = 40;
+        reg = &reghex1;
     } else {
-        char hex[24];
-        hex[23] = 0;
+        limit = 23;
+        reg = &reghex2;
+    }
 
-        // find hexstring and copy to hex, return -1 if no matches
-        int match = regexec(&reghex2, line, 1, pmatch, 0);
-        if (match) return -1;
-        strncpy(hex, line + pmatch[0].rm_so, 23);
+    hex[limit] = 0;
 
-        // trim line to find file
-        trim(line, pmatch[0].rm_so);
+    // find hexstring and copy to hex, return -1 if no matches
+    int match = regexec(reg, line, 1, pmatch, 0);
+    if (match) return -1;
+    strncpy(hex, line + pmatch[0].rm_so, limit);
+
+    // trim line to find file
+    trim(line, pmatch[0].rm_so);
+
+    // copy to path and concat
+    char path[PATH_MAX];
+    strcpy(path, line);
+    concat_path(dir, path);
+    if (!check_exists(path, 1)) return -2;
+
+    fprintf(stderr, "Checking %s...", get_relative_path(dir, path));
+
+    // construct file_struct to give to hash function
+    struct stat s;
+    stat(path, &s);
+
+    hash_value = hash(path, s.st_size);
+
+    if (ver == 1) {
+        result = (long) strcmp(hash_value, hex);
+    } else {
         long size;
         sscanf(line + pmatch[0].rm_eo, "%ld", &size);
 
-        // copy to path and concat
-        char path[PATH_MAX];
-        strcpy(path, line);
-        concat_path(dir, path);
-        if (!check_exists(path, 1)) return -2;
-
-        fprintf(stderr, "Checking %s...", get_relative_path(dir, path));
-
-        // construct file_struct to give to hash function
-        struct stat s;
-        stat(path, &s);
-        hash_value = hash(path, s.st_size);
-
         char temp[24];
         obf_hash(hash_value, temp);
-        printf("Obfuscated hash: %s, expected size: %ld, real size: %ld\n", temp, size, s.st_size);
-        result = strcmp(temp, hex);
+        result = (long) strcmp(temp, hex);
         result |= s.st_size - size;
     }
 
