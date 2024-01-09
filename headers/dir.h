1ff449f93f98bebc73f46d174ebfe91876fafe28
Zachareee <zacharytanhx@yahoo.com>
2024-01-10 00:30:20 +0800

    Added count files function to prevent duplicate files adding to count

--- headers/dir.h
+++ headers/dir.h
@@ -4,3 +4,4 @@ void add_path_to_dir(char *path, dir_t *dir);
 void free_dir(dir_t *dir);
 void write_dir_to_file(dir_t *dir, int level, FILE *f);
 int path_exists(dir_t *dir, const char *path);
+int count_files_in_dir(dir_t *dir);
